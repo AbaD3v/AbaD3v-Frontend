1f7698b7efc9018779b9e95f4b56dbb69d95e742
Terbarys <mynbaevbeibarys230107@gmail.com>
2026-09-16 14:31:28 +0500

    update beibarys member info

--- src/team/members/beibarys-m.ts
+++ src/team/members/beibarys-m.ts
@@ -3,7 +3,7 @@ import type { TeamMember } from '../types';
 const beibarysM: TeamMember = {
   id: 'beibarys-m',
   name: 'Beibarys M',
-  role: 'Участник команды',
+  role: 'Software Engineer, ',
   initials: 'BM',
   github: 'https://github.com/Terbarys',
   avatar: 'https://github.com/Terbarys.png?size=160',

a306e05e175ebcd5f24efba8b489a83c84c973f2
vancitygarfield <kenhanr@gmail.com>
2026-09-16 14:59:01 +0500

    RASULCHIK

--- src/team/members/rasul-k.ts
+++ src/team/members/rasul-k.ts
@@ -4,7 +4,7 @@ const rasulK: TeamMember = {
   id: 'rasul-k',
   name: 'Rasul Kenkhan',
   role: 'Член команды 1',
-  initials: 'RK',
+  initials: 'R.K',
   github: 'https://github.com/vancitygarfield',
   avatar: 'https://github.com/vancitygarfield.png?size=160',
 };

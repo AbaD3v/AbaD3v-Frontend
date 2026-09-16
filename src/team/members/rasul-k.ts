7ad010c8c3ad8dbf813890fb4c7adbf46b1528af
velverose <tlemisovarlan@gmail.com>
2026-09-16 14:56:26 +0500

    rasull

--- src/team/members/rasul-k.ts
+++ src/team/members/rasul-k.ts
@@ -3,7 +3,7 @@ import type { TeamMember } from '../types';
 const rasulK: TeamMember = {
   id: 'rasul-k',
   name: 'Rasul Kenkhan',
-  role: 'Член команды',
+  role: 'Член команды 1',
   initials: 'RK',
   github: 'https://github.com/vancitygarfield',
   avatar: 'https://github.com/vancitygarfield.png?size=160',
